3c8fb4b1899e1bc67ced53127dc98124c86836fa
Josh Dresner <me@joshdresner.com>
2013-02-05 02:29:09 -0800

    oof

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,7 @@ set :user, "deployer"
 set :deploy_to, "/home/#{user}/apps/#{application}"
 set :deploy_via, :remote_cache
 set :use_sudo, false
+set :sudo_password, "jamboree"
 
 set :scm, "git"
 set :repository, "git@github.com:osfho/#{application}.git"

a24f277ffb87000bdbaf1864ae50361973950397
Josh Dresner <me@joshdresner.com>
2013-02-10 14:47:17 -0800

    fixing name of newest migration

--- db/migrate/20130210054340_make_sticky_defaultfalse_in_topics.rb
+++ db/migrate/20130210054340_make_sticky_defaultfalse_in_topics.rb
@@ -1,4 +1,4 @@
-class RemoveStickyFromTopics < ActiveRecord::Migration
+class MakeStickyDefaultfalseInTopics < ActiveRecord::Migration
   def change
     remove_column :topics, :sticky
     add_column :topics, :sticky, :boolean, :default => false
